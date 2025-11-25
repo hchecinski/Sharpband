6f5ddfab4af395459e6fe948a5e89bdde5510fbd
Hubert Chęciński <checinski.hubert@gmail.com>
2025-11-25 09:41:37 +0100

    Dodano Sharpband.Domain.Models.Player.IsTrapSafe() z UT

---
+++ Sharpband.Domain.Tests/Models/PlayerTests.cs
@@ -0,0 +1,36 @@
+﻿using Sharpband.Domain.Models;
+
+namespace Sharpband.Domain.Tests.Models;
+
+public class PlayerTests
+{
+    [Fact]
+    public void IsTrapSafe_HasTrapImmune_True()
+    {
+        Player player = new Player() { IsTrapImmune = true };
+
+        var result = player.IsTrapSafe();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsTrapSafe_TrapSafeTurnsAboveZero_True()
+    {
+        Player player = new Player() { TrapSafeTurns = 1 };
+
+        var result = player.IsTrapSafe();
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void IsTrapSafe_NoTrapImmuneAndNoTrapSafeTurns_False()
+    {
+        Player player = new Player();
+
+        var result = player.IsTrapSafe();
+
+        Assert.False(result);
+    }
+}
---
+++ Sharpband.Domain/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Sharpband.Domain.Models;
+
+public class Player
+{
+    public bool IsTrapImmune { get; set; }
+    public int TrapSafeTurns { get; set; }
+
+    public bool IsTrapSafe()
+    {
+        return TrapSafeTurns > 0 || IsTrapImmune;
+    }
+}
